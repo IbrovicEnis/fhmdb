5c2173c1550185efc9c9930f2241206e9b724e37
urosm <um.uros20@gmail.com>
2024-04-13 00:35:11 +0200

    added proper dialog to CountMoviesFrom and LongstMovieTitel

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -168,7 +168,6 @@ public class HomeController implements Initializable {
                 .max()
                 .orElse(0);
     }
-
     public long countMoviesFrom(List<Movie> filteredMovies, String directors) {
         if (filteredMovies == null || filteredMovies.isEmpty()) {
             return 0;
